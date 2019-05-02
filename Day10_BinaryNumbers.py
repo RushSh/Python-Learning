3f179ce7d7afa85b584f9a65497215423ec6416b
shahr <rshah8@students.kennesaw.edu>
2019-05-01 21:49:15 -0400

    Day10

---
+++ Day10_BinaryNumbers.py
@@ -0,0 +1,10 @@
+#Objective 
+#Binary numbers
+
+#Task 
+#Given a base10 integer, n , convert it to binary (base-2)
+#Then find and print the base-10 integer denoting the 
+#maximum number of consecutive 1's in n's binary representation
+
+decimal = int(input())
+print("{0:#b}".format(decimal))
